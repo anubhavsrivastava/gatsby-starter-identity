fccd18c82b2e1ce30336602a8d09fe9bf9ef2776
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-13 20:36:30 +0530

    fixed pathPrefix for identity

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-identity`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Anubhav Srivastava',
   heading: 'Web Developer',
   // social
